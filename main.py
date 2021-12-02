f6152d729974799a25c614aa56aee75539f4f5b1
LouisT123 <52509987+LouisT123@users.noreply.github.com>
2021-12-02 00:26:12 +0000

    command no longer case sensitive

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ async def on_message(message):
     await message.channel.send('<:hiya:807336709993529374>')
   if message.content.startswith('~hey rei'):
     await message.channel.send('Hey!')
-  if "rei you seeing this shit?" in message.content:
+  if "rei you seeing this shit?" in message.content.lower():
     await message.channel.send('<:ReiStare:887162572963475506>')
   #if message.content.startswith('~rei you seeing this shit?'):
     #await message.channel.send('<:ReiStare:887162572963475506>')
-  if "hey rei, get me the event schedule" in message.content:
+  if "hey rei, get me the event schedule" in message.content.lower():
     await message.channel.send("here you go! https://kenofnz.github.io/priconne-en-event-timer/")
-  if "hey rei, get me the ue spreadsheet" in message.content:
+  if "hey rei, get me the ue spreadsheet" in message.content.lower():
     await message.channel.send("here you go! https://docs.google.com/spreadsheets/d/1JXbzIF4dWqzmmBwAxuNp74_v8eSM0tuDehWZtN9-lxY/edit#gid=504351475")
   #if "hey rei, flip a coin" in message.content:
     
