fc1dc60b333769ca91b6e3f3b416bf589c518311
Spidre <52509987+LouisT123@users.noreply.github.com>
2021-12-20 01:14:36 +0000

    dependencies installed for notification (currently stable)

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-my_secret = os.environ['TOKEN']
-CONFIG = {
-    "bot_token": "my_secret",
-
-    "notification_channelid": 912532987671900180,
-}
--- run.py
+++
@@ -1,9 +0,0 @@
-from Priconne.main import Priconne
-from config import CONFIG
-import os
-from keep_alive import keep_alive
-my_secret = os.environ['TOKEN']
-
-priconne = Priconne(CONFIG)
-priconne.run(CONFIG["my_secret"])
-keep_alive()
