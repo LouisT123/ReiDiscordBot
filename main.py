dbefd1240a5c3bbcd33ce31f611137427806a3b5
LouisT123 <louistaing@aol.com>
2022-03-15 18:06:49 -0700

    [STABLE] Added boss ping notification

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 #from keep_alive import keep_alive
 import random
 from dotenv import load_dotenv
-client = discord.Client()
+#client = discord.Client()
+# 3/15 replacing above to add intents to see if can find member
+intents = discord.Intents.all()
+client = discord.Client(intents = intents)
 from notification import Notification 
 #notification bot, takes in chat id and client
 notif = Notification(client, 785705961032515614 )
@@ -18,6 +21,7 @@ async def on_ready():
   print('we have logged in as {0.user}'.format(client))
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=": ~commands"))
   notif.start()
+
 
 
 #if message is recieved
@@ -56,7 +60,82 @@ async def on_message(message):
   if message.content.startswith("~commands"):
     await message.channel.send("Here are my commands!\nrei flip a coin\nhey rei get me the event schedule\nhey rei get me the ue guide\nhey rei get me the luna tower guide\nhey rei get me the gear guide\nguides will always be up to date\nrei you seeing this shit?" )
 
+#Working! 3/15 boss ping
+@client.event
+#add the roles
+async def on_raw_reaction_add(payload):
+  message_id = payload.message_id
+  #if user reacts to the specific select roles message on discord
+  if message_id == 953428332530245632:
+    #grab guild id 
+    guild_id = payload.guild_id
+    #search for all the guilds to find matching one
+    guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+    #get role
+    if payload.emoji.name == 'yabaidesune':
+      role = discord.utils.get(guild.roles, name='b1')
+    elif payload.emoji.name == 'kyaru':
+      role = discord.utils.get(guild.roles, name='b2')
+    elif payload.emoji.name == 'disturbed':
+      role = discord.utils.get(guild.roles, name='b3')
+    elif payload.emoji.name == 'criyaru':
+      role = discord.utils.get(guild.roles, name='b4')
+    elif payload.emoji.name == 'yamete':
+      role = discord.utils.get(guild.roles, name='b5')
+    else:
+      role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+    #if we got the role, attach it to the user
+    if role is not None:
+      member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+      #if valid add the role
+      if member is not None:
+        await member.add_roles(role)
+        print("role is added ")
+      else:
+        print("member not found")
+    else:
+      print("Role not found")
+
+
+@client.event
+#remove the roles
+async def on_raw_reaction_remove(payload):
+  message_id = payload.message_id
+  #if message is :one:
+  if message_id == 953428332530245632:
+    #grab guild id 
+    guild_id = payload.guild_id
+    #search for all the guilds to find matching one
+    guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+    #get role
+    if payload.emoji.name == 'yabaidesune':
+      role = discord.utils.get(guild.roles, name='b1')
+    elif payload.emoji.name == 'kyaru':
+      role = discord.utils.get(guild.roles, name='b2')
+    elif payload.emoji.name == 'disturbed':
+      role = discord.utils.get(guild.roles, name='b3')
+    elif payload.emoji.name == 'criyaru':
+      role = discord.utils.get(guild.roles, name='b4')
+    elif payload.emoji.name == 'yamete':
+      role = discord.utils.get(guild.roles, name='b5')
+    else:
+      role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+    #if we got the role, attach it to the user
+    if role is not None:
+      member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+      #if valid remove the role
+      if member is not None:
+        await member.remove_roles(role)
+        print("role is removed ")
+      else:
+        print("member not found")
+    else:
+      print("Role not found")
+
 #keep_alive()
+
 client.run(os.environ['TOKEN'])
 
 
